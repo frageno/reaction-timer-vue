d9441a495e7d406fa5deb102b1aa6e294d0d94ae
Szymon Piątkowski <64953895+frageno@users.noreply.github.com>
2023-04-20 20:07:31 +0200

    reaction timer

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="startPlay" :disabled="isPlaying">Start play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showScore" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+export default {
+  name: "App",
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showScore: false
+    }
+  },
+  methods: {
+    startPlay(){
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.showScore = false
+    },
+    endGame(reactionTime){
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showScore = true
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>Reaction time: {{ this.score }} ms</p>
+        <p>{{ this.rank }}</p>
+    </div>
+</template>
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if(this.score <= 250) {
+            this.rank = 'Fast'
+        } else if (this.score <= 400) {
+            this.rank = 'Slower'
+        } else {
+            this.rank = 'So Sloooow'
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
